7d46c7f40df1186150d40663fc84847a0b477c17
노지원 <@DESKTOP-4P75CIQ>
2022-06-12 19:44:44 +0900

    change

--- HelloGit/src/MyHello.java
+++ HelloGit/src/MyHello.java
@@ -2,7 +2,7 @@
 public class MyHello {
 	public void sayHello() {
 		System.out.println("Hello");
-		System.out.println("sayHello change in master branch");
+		System.out.println("sayHello Change in master branch");
 	}
 
 }
